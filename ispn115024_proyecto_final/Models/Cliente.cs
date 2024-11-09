0800d3d1ec9b9cd500c1b66db511ce415b67bbaf
JGutierrez2294 <isnp115024@ugb.edu.sv>
2024-11-09 03:03:30 -0600

    Add project files.

---
+++ ispn115024_proyecto_final/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ispn115024_proyecto_final.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int idCliente { get; set; }
+
+        public string nombre { get; set; }
+
+        public string direccion { get; set; }
+
+        public string telefono { get; set; }
+
+        public string documento { get; set; }
+    }
+}
---
+++ ispn115024_proyecto_final/Models/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ispn115024_proyecto_final.Models;
+
+namespace ispn115024_proyecto_final.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public int idCliente { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Cliente>().HasKey(Cliente => Cliente.idCliente);
+        }
+
+        public string nombre { get; set; }
+
+        public string direccion { get; set; }
+
+        public string telefono { get; set; }
+
+        public string documento { get; set; }
+        public DbSet<ispn115024_proyecto_final.Models.Cliente> Cliente { get; set; } = default!;
+    }
+}
